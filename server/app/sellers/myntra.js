506a41ca6909a5aca16de72cfbdfb51f74afbf7f
Aakash Goel <aakash.lpin@gmail.com>
2014-07-01 22:16:09 +0530

    scraper fixes for myntra

--- server/app/sellers/myntra.js
+++ server/app/sellers/myntra.js
@@ -2,8 +2,9 @@
 var _ = require('underscore');
 module.exports = function($) {
 	var priceDOM, price;
-	priceDOM = $('.price').find('span').remove();	//might contain offers
-	price = priceDOM.split('Rs.')[1].replace('/\s/g', '').replace(',', '');
+	priceDOM = $('.summary .price');
+	priceDOM.find('span').remove();	//might contain offers
+	price = priceDOM.text().split('Rs.')[1].replace('/\s/g', '').replace(',', '');
 	if (price) {
 		price = parseInt(price, 10);
 	}
@@ -14,7 +15,7 @@ module.exports = function($) {
 
     return {
         price: price,
-        name: $('.title').text().replace(/^\s+|\s+$/g, ''),
+        name: $('.summary .title').text().replace(/^\s+|\s+$/g, ''),
         image: $('meta[itemprop="image"]').attr('content')
     };
 };
