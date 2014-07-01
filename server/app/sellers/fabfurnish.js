6f1e77411ba8f220520fe1adec31daa8a7ddf54a
Aakash Goel <aakash.lpin@gmail.com>
2014-07-01 16:16:09 +0530

    fabfurnish support

---
+++ server/app/sellers/fabfurnish.js
@@ -0,0 +1,17 @@
+'use strict';
+var _ = require('underscore');
+module.exports = function($) {
+	var price = $('[itemprop="price"]').attr('content').replace(',', '');
+	price = parseInt(price, 10);
+	if (_.isNaN(price)) {
+		price = false;
+	}
+
+	var imageDOM = $('#imgThumb_1 img');
+
+    return {
+        price: price,
+        name: $('[itemprop="name"]').text().replace(/^\s+|\s+$/g, ''),
+        image: imageDOM.attr('longdesc')
+    };
+};
