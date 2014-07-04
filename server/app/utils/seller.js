e5c9e0c26583e52ba97ad8e72dcac863a49d22c8
Aakash Goel <aakash.lpin@gmail.com>
2014-07-04 13:32:26 +0530

    better getSellerFromURL method

--- server/app/utils/seller.js
+++ server/app/utils/seller.js
@@ -1,27 +1,16 @@
 'use strict';
 var mongoose = require('mongoose');
+var config = require('../../config/config');
+var _ = require('underscore');
+
 module.exports = {
     getSellerFromURL: function(url) {
-        if (url.indexOf('flipkart.com') >= 0) {
-            return 'flipkart';
-        } else if (url.indexOf('amazon.in') >= 0) {
-            return 'amazon';
-        } else if (url.indexOf('jabong.com') >= 0) {
-            return 'jabong';
-        } else if (url.indexOf('myntra.com') >= 0) {
-            return 'myntra';
-        } else if (url.indexOf('infibeam.com') >= 0) {
-            return 'infibeam';
-        } else if (url.indexOf('snapdeal.com') >= 0) {
-            return 'snapdeal';
-        } else if (url.indexOf('fabfurnish.com') >= 0) {
-            return 'fabfurnish';
-        } else if (url.indexOf('bajaao.com') >= 0) {
-            return 'bajaao';
-        } else if (url.indexOf('pepperfry.com') >= 0) {
-            return 'pepperfry';
-        }
-        return null;
+        var sellers = _.keys(config.sellers);
+        return _.find(sellers, function(seller) {
+            if (url.indexOf(config.sellers[seller].url) >=0 ) {
+                return seller;
+            }
+        });
     },
     getSellerJobModelInstance: function(seller) {
         var jobSellerModelName = seller + '_job';
