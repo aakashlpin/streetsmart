e451aa3bfd0d3cdfc06cac5d9f23c5ac18b418f0
Aakash Goel <aakash.lpin@gmail.com>
2014-07-07 17:29:32 +0530

    TODO: log processing

--- server/logger.js
+++ server/logger.js
@@ -5,4 +5,29 @@ winston.add(winston.transports.DailyRotateFile, {
     handleExceptions: true
 });
 
+function processLogs() {
+	//this z gotta be called from a cronjob and put in some kind of kue
+	var options = {
+	    from: new Date - 24 * 60 * 60 * 1000,
+	    until: new Date,
+	    limit: 10,
+	    start: 0,
+	    order: 'desc',
+	    fields: ['message']
+	  };
+  	winston.query(options, function(err, results) {
+  		if (err) {
+  			throw err;
+  		}
+
+  		console.log(results);
+
+  		//TODO check this out
+
+  		//write to a file this results json
+  		//send to self an email with attachment of this file
+  	})
+}
+
 exports.logger = winston;
+exports.logsProcessor = processLogs;
