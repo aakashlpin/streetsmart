26d69dc65a7b5e236426c8248ce142d80c48d520
Aakash Goel <aakash.lpin@gmail.com>
2015-05-09 16:02:20 +0530

    increase cron tab times

--- server/config/config.js
+++ server/config/config.js
@@ -59,7 +59,7 @@ var commonConfig = {
             hasDeepLinking: true,
             cronPattern: {
                 'development': '0-59/1 * * * *',
-                'production': '0 0-23/1 * * *'
+                'production': '0 0-23/2 * * *'
             }
         },
         amazon: {
@@ -72,7 +72,7 @@ var commonConfig = {
             hasDeepLinking: true,
             cronPattern: {
                 'development': '0-59/3 * * * *',
-                'production': '15 0-23/1 * * *'
+                'production': '30 0-23/2 * * *'
             }
         },
         myntra: {
