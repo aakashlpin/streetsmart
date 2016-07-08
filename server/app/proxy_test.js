b20e5d903cca3dafa6325d309d324ba6cd882271
Aakash Goel <aakash.lpin@gmail.com>
2016-07-08 20:54:18 +0530

    updated proxy test

--- server/app/proxy_test.js
+++ server/app/proxy_test.js
@@ -1,9 +1,13 @@
 var request = require('request');
+var fs = require('fs');
 
 request({
   url: 'http://www.amazon.in/Samsung-FH4003-inches-Ready-Black/dp/B00URFD45G',
-  proxy: 'http://e8ed50e08e444e22a3f13b546196a2c7:@proxy.crawlera.com:8010'
+  proxy: 'http://e8ed50e08e444e22a3f13b546196a2c7:@proxy.crawlera.com:8010',
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
+  }
 }, function (error, response, body) {
   console.log(response.statusCode);
-  console.log(body);
+  fs.writeFileSync('content.html', body);
 })
