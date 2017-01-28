63e76a02398359d1c61016ae783dc121cbac2146
Aakash Goel <aakash.lpin@gmail.com>
2017-01-28 15:56:44 +0530

    added shipit config

---
+++ shipitfile.js
@@ -0,0 +1,28 @@
+module.exports = function (shipit) {
+  require('shipit-deploy')(shipit);
+
+  shipit.initConfig({
+    default: {
+      workspace: '/tmp/github-monitor',
+      deployTo: '/tmp/deploy_to',
+      branch: 'upgrade/everthing',
+      repositoryUrl: 'https://github.com/aakashlpin/streetsmart.git',
+      ignores: ['.git', 'node_modules'],
+      rsync: ['--del'],
+      keepReleases: 2,
+      key: '/Users/aakashlpin/.ssh/id_rsa',
+      shallowClone: true
+    },
+    production: {
+      servers: 'aakashlpin@139.59.10.255'
+    }
+  });
+
+  shipit.task('start', function () {
+    return shipit.remote('cd /tmp/deploy_to/current && yarn && bower install && grunt').then(function () {
+      shipit.remote('cd /tmp/deploy_to/current/server && yarn && npm start').then(function () {
+        shipit.log('all done');
+      })
+    })
+  })
+}
