db08608b38b5a73b5d41ac0c7cfa71a4a7b0d890
Aakash Goel <aakash.lpin@gmail.com>
2017-03-14 22:54:38 +0530

    updated shipit

--- shipitfile.js
+++ shipitfile.js
@@ -5,7 +5,7 @@ module.exports = function (shipit) {
     default: {
       workspace: '/tmp/github-monitor',
       deployTo: '/tmp/deploy_to',
-      branch: 'master',
+      branch: 'feature/queue-split',
       repositoryUrl: 'https://github.com/aakashlpin/streetsmart.git',
       ignores: ['.git', 'node_modules'],
       rsync: ['--del'],
@@ -14,16 +14,13 @@ module.exports = function (shipit) {
       shallowClone: true
     },
     production: {
-      servers: 'aakashlpin@mongo1.cheapass.in'
-    },
-    newproduction: {
       servers: 'aakashlpin@mongo3.cheapass.in'
     },
   });
 
   shipit.task('start', function () {
     return shipit.remote('cd /tmp/deploy_to/current && yarn && bower install && grunt').then(function () {
-      shipit.remote('cd /tmp/deploy_to/current/server && yarn && mkdir logs && forever stopall && npm start').then(function () {
+      shipit.remote('cd /tmp/deploy_to/current/server && yarn && mkdir logs && forever stopall && mv .production.env .env && npm start').then(function () {
         shipit.log('all done');
       })
     })
