607dc1269826ab946135e8464551ed3b092b469d
Aakash Goel <aakash.lpin@gmail.com>
2017-03-16 18:53:14 +0530

    updated shipit

--- shipitfile.js
+++ shipitfile.js
@@ -5,7 +5,7 @@ module.exports = function (shipit) {
     default: {
       workspace: '/tmp/github-monitor',
       deployTo: '/tmp/deploy_to',
-      branch: 'feature/queue-split',
+      branch: 'master',
       repositoryUrl: 'https://github.com/aakashlpin/streetsmart.git',
       ignores: ['.git', 'node_modules'],
       rsync: ['--del'],
