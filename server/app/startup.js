bd16194021d8df8fcdf52f201d00893b5712d4af
Aakash Goel <aakash.lpin@gmail.com>
2017-03-23 21:10:54 +0530

    lets try with a slow queue

--- server/app/startup.js
+++ server/app/startup.js
@@ -13,79 +13,88 @@ const { queue, getSellerQueueKey, getUserJobsQueueNameForSeller } = queueLib;
 const { sellers } = config;
 const sellerKeys = Object.keys(sellers);
 
+function setupCronFromSeller(seller) {
+  const sellerModelKey = seller === 'amazonSlow' ? 'amazon' : seller;
+  const jobQueueName = getSellerQueueKey(seller);
+  const sellerConfig = config.sellers[seller];
+
+  logger.log(`Booting: Setting up ${seller} cron tab ${process.env[sellerConfig.cronKey]}`);
+
+  // Setup cron jobs
+  new CronJob({
+    cronTime: process.env[sellerConfig.cronKey],
+    start: true,
+    timeZone: 'Asia/Kolkata',
+    onTick: () => {
+      logger.log(`cron ticking for ${seller}`);
+      queue.inactiveCount(jobQueueName, (err, total) => {
+        logger.log({ err, inactiveCount: total });
+        if (err) {
+          return logger.log('error', `error getting queue.inactiveCount on ${jobQueueName}`, err);
+        }
+
+        if (total) {
+          return logger.log(`not processing cron tick because ${total} items in ${jobQueueName} queue.`);
+        }
+
+        const SellerJobModel = sellerUtils.getSellerJobModelInstance(sellerModelKey);
+        SellerJobModel.get((err, sellerJobs) => {
+          if (err) {
+            return logger.log('error', `CRITICAL: error in doing SellerJobModel.get on ${SellerJobModel.modelName}`);
+          }
+
+          if (!sellerJobs) {
+            return logger.log('warning', `cron tick: no seller jobs for ${seller}`);
+          }
+
+          const sellerJobsMappedWithSeller = sellerJobs.map(sellerJob =>
+            _.extend({}, sellerJob, {
+              seller,
+              title: `Processing ${sellerJob.productName}`,
+            })
+          );
+
+          logger.log(`adding ${sellerJobsMappedWithSeller.length} items to ${seller} queue`);
+
+          sellerJobsMappedWithSeller.forEach((jobData) => {
+            queue
+            .create(jobQueueName, jobData)
+            .removeOnComplete(true)
+            .priority(process.env.ADMIN_EMAIL_IDS.indexOf(jobData.email) > -1 ? 'high' : 'normal')
+            .save((saveErr) => {
+              if (saveErr) {
+                return logger.log('error', 'Unable to add job to queue', saveErr);
+              }
+              return logger.log('Added job to queue', jobQueueName, jobData);
+            });
+          });
+        });
+      });
+    },
+  });
+
+  // Start processing existing jobs
+  const concurrency = sellerConfig.concurrency || 4;
+  queue.process(jobQueueName, concurrency, (job, done) => {
+    if (sellerConfig.keepSlow) {
+      setTimeout(() => {
+        processJob(job, done);
+      }, 10 * 1000);
+    } else {
+      processJob(job, done);
+    }
+  });
+}
+
 if (process.env.IS_CRON_ACTIVE) {
   const activeSellerKeys =
     sellerKeys
       .filter(key => sellers[key].isCronActive);
 
-
-  activeSellerKeys.forEach((seller) => {
-    const jobQueueName = getSellerQueueKey(seller);
-    const sellerConfig = config.sellers[seller];
-
-    logger.log(`Booting: Setting up ${seller} cron tab ${process.env[sellerConfig.cronKey]}`);
-
-    // Setup cron jobs
-    new CronJob({
-      cronTime: process.env[sellerConfig.cronKey],
-      start: true,
-      timeZone: 'Asia/Kolkata',
-      onTick: () => {
-        logger.log(`cron ticking for ${seller}`);
-        queue.inactiveCount(jobQueueName, (err, total) => {
-          logger.log({ err, inactiveCount: total });
-          if (err) {
-            return logger.log('error', `error getting queue.inactiveCount on ${jobQueueName}`, err);
-          }
-
-          if (total) {
-            return logger.log(`not processing cron tick because ${total} items in ${jobQueueName} queue.`);
-          }
-
-          const SellerJobModel = sellerUtils.getSellerJobModelInstance(seller);
-          SellerJobModel.get((err, sellerJobs) => {
-            if (err) {
-              return logger.log('error', `CRITICAL: error in doing SellerJobModel.get on ${SellerJobModel.modelName}`);
-            }
-
-            if (!sellerJobs) {
-              return logger.log('warning', `cron tick: no seller jobs for ${seller}`);
-            }
-
-            const sellerJobsMappedWithSeller = sellerJobs.map(sellerJob =>
-              _.extend({}, sellerJob, {
-                seller,
-                title: `Processing ${sellerJob.productName}`,
-              })
-            );
-
-            logger.log(`adding ${sellerJobsMappedWithSeller.length} items to ${seller} queue`);
-
-            sellerJobsMappedWithSeller.forEach((jobData) => {
-              queue
-              .create(jobQueueName, jobData)
-              .removeOnComplete(true)
-              .priority(process.env.ADMIN_EMAIL_IDS.indexOf(jobData.email) > -1 ? 'high' : 'normal')
-              .save((saveErr) => {
-                if (saveErr) {
-                  return logger.log('error', 'Unable to add job to queue', saveErr);
-                }
-                return logger.log('Added job to queue', jobQueueName, jobData);
-              });
-            });
-          });
-        });
-      },
-    });
-
-    // Start processing existing jobs
-    const concurrency = sellerConfig.hasMicroService ? 1 : 4;
-    queue.process(jobQueueName, concurrency, (job, done) =>
-      processJob(job, done)
-    );
-  });
+  activeSellerKeys.forEach(seller => setupCronFromSeller(seller));
 }
 
+// start processing items added from UI
 sellerKeys.forEach((seller) => {
   const queueName = getUserJobsQueueNameForSeller(seller);
   logger.log('info', 'setting up user job queue processing', queueName);
@@ -96,7 +105,7 @@ sellerKeys.forEach((seller) => {
 });
 
 queue.on('error', (err) => {
-  logger.log('Oops..', err);
+  logger.log('error', 'CRITICAL!! queue error', err);
 });
 
 
