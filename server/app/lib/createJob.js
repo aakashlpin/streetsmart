7e4083f0c01224a1044d9ec8cdc94c26f645e39b
Aakash Goel <aakash.lpin@gmail.com>
2017-03-29 19:31:20 +0530

    updated the create job to handle the new way of doing things

--- server/app/lib/createJob.js
+++ server/app/lib/createJob.js
@@ -3,92 +3,146 @@ const mongoose = require('mongoose');
 const logger = require('../../logger').logger;
 const Emails = require('../controllers/emails');
 const sellerUtils = require('../utils/seller');
+const redis = require('redis');
+const dbConnections = require('./dbConnections');
 
-const User = mongoose.model('User');
-const Job = mongoose.model('Job');
+const { jobModelConnections, priceHistoryConnections } = dbConnections;
+const UserModel = mongoose.model('User');
+const redisClient = redis.createClient();
+
+// data =>
+// {
+//     email: String,
+//     currentPrice: Number,
+//     productName: String,
+//     productURL: String,
+//     productImage: String,
+//     seller: String,
+//     source: String
+// }
 
 function createJob(data, callback) {
-    // data =>
-    // {
-    //     email: String,
-    //     currentPrice: Number,
-    //     productName: String,
-    //     productURL: String,
-    //     productImage: String,
-    //     seller: String,
-    //     source: String
-    // }
-
   _.extend(data, {
     currentPrice: data.currentPrice || data.productPrice,
   });
 
-  User.findOne({ email: data.email }, (err, userQueryResult) => {
+  const {
+    email,
+    currentPrice,
+    productImage,
+    productName,
+    productURL,
+    seller,
+    source,
+  } = data;
+
+  UserModel.findOne({ email }, { _id: 1, email: 1, suspended: 1 }, (err, user) => {
     if (err) {
-      logger.log('error', 'error finding email in user model', { error: err, email: data.email });
-      return;
-    }
-
-    const isEmailVerified = userQueryResult ? (!!userQueryResult.email) : false;
-    const emailObject = { email: data.email };
-    let responseMessage = '';
-    let responseCode;
-
-    if (!isEmailVerified) {
-      logger.log('info', 'new user unverified', { email: data.email });
-      responseMessage = 'Please verify your email id to activate this alert.';
-      responseCode = 'pending';
-
-      Emails.sendVerifier(emailObject, data, (err, status) => {
-        if (err) {
-          logger.log('error', 'error sending verification email', { error: err, email: emailObject.email });
-          return;
-        }
-        logger.log('info', 'verification email triggered', { status, email: emailObject.email });
+      logger.log('error', 'error finding email in user model', {
+        error: err,
+        email,
       });
-    } else {
-      logger.log('info', 'returning user', { email: userQueryResult.email });
-      responseMessage = 'Awesome! Price drop alert activated.';
-      responseCode = 'verified';
+
+      return callback(err);
     }
 
     const jobData = {
-      email: emailObject.email,
-      currentPrice: data.currentPrice,
-      productName: data.productName,
-      productURL: data.productURL,
-      productImage: data.productImage,
-      seller: data.seller,
-      source: data.source,
-      isEmailVerified,    // this is needed for some checks while creating a new job
+      email,
+      currentPrice,
+      productName,
+      productURL,
+      productImage,
+      seller,
+      source,
+      suspended: !user,
+      createdAt: new Date(),
     };
 
-    Job.post({ query: jobData }, (err, createdJob) => {
-      if (err || !createdJob) {
-        logger.log('error', 'error adding job to db', { error: err });
-        return callback(null, {
-          status: err,
-          code: 'error',
-        });
-      }
+    const SellerJobModel = jobModelConnections[seller];
+    const SellerPriceHistoryModel = priceHistoryConnections[seller];
+    SellerJobModel.collection.update({ email, productURL }, jobData, { upsert: true },
+      (err, reply) => {
+        if (err) {
+          logger.log('error', 'unable to upsert', jobData, err);
+          return callback(err);
+        }
 
-      // increase the products counter in the db
-      sellerUtils.increaseCounter('itemsTracked');
-      callback(null, {
-        status: responseMessage,
-        code: responseCode,
+        if (reply.result.nModified) {
+          // however, the entry gets updated with createdAt timestamp
+          logger.log('job already exists', jobData);
+        }
+
+        if (reply.result.upserted && reply.result.upserted.length) {
+          const { _id } = reply.result.upserted[0];
+          logger.log('job upserted', jobData);
+          SellerPriceHistoryModel.collection.insert({
+            jobId: _id,
+            email,
+            productURL,
+            date: new Date(),
+            price: currentPrice,
+          });
+
+          sellerUtils.increaseCounter('itemsTracked');
+          redisClient.zincrby('emailAlertsSet', 1, email);
+        }
+      }
+    );
+
+    if (!user) {
+      // new user flow
+      logger.log('new user unverified', { email, jobData });
+      UserModel.collection.insert({ email, suspended: true }, (err, doc) => {
+        if (err) {
+          logger.log('error', 'unable to insert to user collection', err);
+          return callback(err);
+        }
+
+        const _id = doc.ops[0]._id;
+
+        Emails.sendVerifier({ email, _id }, data, (err, status) => {
+          if (err) {
+            return logger.log('error', 'error sending verification email', {
+              error: err,
+              email,
+            });
+          }
+          logger.log('info', 'verification email triggered', { status, email });
+        });
+      });
+      return callback(null, {
+        status: 'ok',
+      });
+    }
+
+    if (user && user.suspended) {
+      logger.log('newish user unverified and inactive', { email, jobData });
+
+      Emails.sendVerifier({ email, _id: user._id }, data, (err, status) => {
+        if (err) {
+          return logger.log('error', 'error sending verification email', {
+            error: err,
+            email,
+          });
+        }
+        logger.log('info', 'verification email triggered', { status, email });
       });
 
-      if (isEmailVerified) {
-        emailObject._id = userQueryResult._id;
-        Emails.sendHandshake(emailObject, data, (err, status) => {
-          if (err) {
-            logger.log('error', 'error sending acceptance email', { error: err, email: emailObject.email });
-            return;
-          }
-          logger.log('info', 'acceptance email triggered', { status, email: emailObject.email });
-        });
+      return;
+    }
+
+    // existing user flow
+    logger.log('returning user', { email, jobData });
+    Emails.sendHandshake({ email, _id: user._id }, data, (err, status) => {
+      if (err) {
+        logger.log('error', 'error sending acceptance email', { error: err, email });
+        return;
       }
+      logger.log('info', 'acceptance email triggered', { status, email });
+    });
+
+    return callback(null, {
+      status: 'ok',
     });
   });
 }
