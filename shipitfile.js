37dc317473d2b1e9de92a4b9eeb53d1cd7bf3e67
Aakash Goel <aakash.lpin@gmail.com>
2017-07-02 21:55:35 +0530

    updated shipit config

--- shipitfile.js
+++ shipitfile.js
@@ -5,7 +5,7 @@ module.exports = function (shipit) {
     default: {
       workspace: '/tmp/github-monitor',
       deployTo: '/tmp/deploy_to',
-      branch: 'feature/blog-embed',
+      branch: 'master',
       repositoryUrl: 'https://github.com/aakashlpin/streetsmart.git',
       ignores: ['.git', 'node_modules'],
       rsync: ['--del'],
