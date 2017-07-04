5b65aef8fb3d966bcf03c1b14ac12fe97b526fd0
Aakash Goel <aakash.lpin@gmail.com>
2017-07-04 23:21:35 +0530

    enable kue UI on prod

--- server/app/lib/queue.js
+++ server/app/lib/queue.js
@@ -2,9 +2,9 @@ const kue = require('kue');
 
 const queue = kue.createQueue();
 
-if (process.env.IS_DEV) {
-  kue.app.listen(process.env.KUE_PORT);
-}
+// if (process.env.IS_DEV) {
+// }
+kue.app.listen(process.env.KUE_PORT);
 
 function getSellerQueueKey(seller) {
   return `scraper-${seller}`;
